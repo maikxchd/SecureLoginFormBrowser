809547aee6c06c1677cf4bb50f960d7aa412b13e
maik <36390867+maikxchd@users.noreply.github.com>
2023-01-05 22:33:21 +0700

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -9,27 +9,36 @@
 <body>
 <?php      
     include('connection.php');  
+      
+    // Prepare a statement for execution
+    $stmt = mysqli_prepare($con, "SELECT * FROM login WHERE username = ? AND password = ?");
+      
+    // Bind parameters to the statement
+    mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+      
+    // Set the variables
     $username = $_POST['user'];  
     $password = $_POST['pass'];  
       
-        //to prevent from mysqli injection  
-        $username = stripcslashes($username);  
-        $password = stripcslashes($password);  
-        $username = mysqli_real_escape_string($con, $username);  
-        $password = mysqli_real_escape_string($con, $password);  
+    // Execute the statement
+    mysqli_stmt_execute($stmt);
       
-        $sql = "select *from login where username = '$username' and password = '$password'";  
-        $result = mysqli_query($con, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-        $count = mysqli_num_rows($result);  
+    // Get the result
+    $result = mysqli_stmt_get_result($stmt);  
+      
+    // Fetch the row
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+      
+    // Get the number of rows returned
+    $count = mysqli_num_rows($result);  
           
-        if($count == 1){  
-            echo "<h1><center> Your Account Has Been Secured </center></h1>";  
-        }  
-        else{  
-            echo "<h1> Login failed. Invalid username or password.</h1>";  
-        }     
-?>  
+    if($count == 1){  
+        echo "<h1><center> Your Account Has Been Secured </center></h1>";  
+    }  
+    else{  
+        echo "<h1> Login failed. Invalid username or password.</h1>";  
+    }     
+?> 
 </body>
 </html>
 
